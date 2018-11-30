75df7920e9b8074ce9c1ab75d1624a4051ea92a8
Frank Young <frankyoung0305@gmail.com>
2018-11-30 16:22:14 +0800

    add UITextFiled

--- HynopsisNerd/TTHypnosisViewController.m
+++ HynopsisNerd/TTHypnosisViewController.m
@@ -8,7 +8,7 @@
 
 #import "TTHypnosisViewController.h"
 #import "TTHypnosisView.h"
-@interface TTHypnosisViewController ()
+@interface TTHypnosisViewController () <UITextFieldDelegate>
 @property (nonatomic, strong) UISegmentedControl *segmCtl;
 @property (nonatomic, strong) TTHypnosisView *backgroundView;
 @end
@@ -19,6 +19,27 @@
     self.backgroundView = [[TTHypnosisView alloc] initWithFrame:[UIApplication sharedApplication].keyWindow.bounds];
     self.view = self.backgroundView;
 //    NSLog(@"%f", self.view.bounds.size.width);
+    CGRect frame = [UIScreen mainScreen].bounds;
+
+    
+    NSArray *arry = @[@"red", @"green", @"blue"];
+//    NSArray *arry = [NSArray arrayWithObjects:@"red", @"green", @"blue", nil];
+    self.segmCtl = [[UISegmentedControl alloc] initWithItems:arry];
+    //    NSLog(@"%f", self.view.bounds.size.width);
+    self.segmCtl.frame = CGRectMake(10, 50, self.view.bounds.size.width-20, 50);
+    //    NSLog(@"%f,%f,%f,%f", self.segmCtl.frame.origin.x, self.segmCtl.frame.origin.y, self.segmCtl.frame.size.width, self.segmCtl.frame.size.height);
+    //    self.segmCtl.tintColor = [UIColor redColor];
+    [self.segmCtl addTarget:self action:@selector(changeColor:) forControlEvents:UIControlEventValueChanged];
+    [self.view addSubview:self.segmCtl];
+    
+    CGRect textFieldRect = CGRectMake(40, 120, frame.size.width-80, 30);
+    UITextField *textField = [[UITextField alloc]initWithFrame:textFieldRect];
+    textField.borderStyle = UITextBorderStyleRoundedRect;
+    textField.placeholder = @"Hynopsis me";
+    textField.returnKeyType = UIReturnKeyDone;
+    textField.delegate = self;
+    
+    [self.backgroundView addSubview:textField];
     
 }
 
@@ -34,17 +55,7 @@
 }
 
 - (void)viewDidLoad{
-//    NSArray *arry = @[@"red", @"green", @"blue"];
-    NSArray *arry = [NSArray arrayWithObjects:@"red", @"green", @"blue", nil];
-    self.segmCtl = [[UISegmentedControl alloc] initWithItems:arry];
-//    NSLog(@"%f", self.view.bounds.size.width);
-    self.segmCtl.frame = CGRectMake(10, 50, self.view.bounds.size.width-20, 50);
-    
-//    NSLog(@"%f,%f,%f,%f", self.segmCtl.frame.origin.x, self.segmCtl.frame.origin.y, self.segmCtl.frame.size.width, self.segmCtl.frame.size.height);
-//    self.segmCtl.tintColor = [UIColor redColor];
-    [self.segmCtl addTarget:self action:@selector(changeColor:) forControlEvents:UIControlEventValueChanged];
-    
-    [self.view addSubview:self.segmCtl];
+
 
 }
 
@@ -70,5 +81,51 @@
             break;
     }
 }
+- (void)drawHypnosisMessages:(NSString *)message{
+    for(int i = 0; i < 20; i++){
+        UILabel *messageLable = [[UILabel alloc] init];
+        messageLable.backgroundColor = [UIColor clearColor];
+        messageLable.textColor = [UIColor whiteColor];
+        messageLable.text = message;
+        
+        [messageLable sizeToFit];
+        int width = (int)(self.view.bounds.size.width - messageLable.bounds.size.width);
+        int x = arc4random() % width;
+        int height = (int)(self.view.bounds.size.height - messageLable.bounds.size.height);
+        int y = arc4random() % height;
+        
+        CGRect frame = messageLable.frame;
+        frame.origin = CGPointMake(x, y);
+        messageLable.frame = frame;
+        
+        [self.view addSubview:messageLable];
+        UIInterpolatingMotionEffect *motionEffect;
+        motionEffect = [[UIInterpolatingMotionEffect alloc] initWithKeyPath:@"center.x" type:UIInterpolatingMotionEffectTypeTiltAlongHorizontalAxis];
+        motionEffect.minimumRelativeValue = @(-25);
+        motionEffect.maximumRelativeValue = @(25);
+        [messageLable addMotionEffect:motionEffect];
+        
+        motionEffect = [[UIInterpolatingMotionEffect alloc] initWithKeyPath:@"center.y" type:UIInterpolatingMotionEffectTypeTiltAlongVerticalAxis];
+        motionEffect.minimumRelativeValue = @(-25);
+        motionEffect.maximumRelativeValue = @(25);
+        [messageLable addMotionEffect:motionEffect];
+        
+    }
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField{
+    NSLog(@"%@", textField.text);
+    [self drawHypnosisMessages:textField.text];
+    
+    textField.text = @"";
+    [textField resignFirstResponder];
+    return YES;
+}
+
+//- (BOOL)respondsToSelector:(SEL)aSelector{
+//    NSLog(@"here");
+//
+//    return [super respondsToSelector:aSelector];
+//}
 
 @end
